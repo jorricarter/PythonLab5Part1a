ab37e3ceafe97ce1dcedb6240679b0c678e0af54
Jack <jackxthexrabbit@gmail.com>
2018-02-25 21:47:57 -0600

    Completed 'Part 1b'. This commit contains 'Part 1a' + 'Part 1b'. 'Part 2 will be in a separate repository.'

--- miles_db/mileage.py
+++ miles_db/mileage.py
@@ -8,19 +8,20 @@ db_url = 'mileage.db'   # Assumes the table miles have already been created.
     create table miles (vehicle text, total_miles float);
 """
 
+
 class MileageError(Exception):
     pass
 
-def add_miles(vehicle, new_miles):
-    '''If the vehicle is in the database, increment the number of miles by new_miles
-    If the vehicle is not in the database, add the vehicle and set the number of miles to new_miles
 
-    If the vehicle is None or new_miles is not a positive number, raise MileageError
-    '''
+def add_miles(vehicle, new_miles):
+    """If the vehicle is in the database, increment the number of miles by new_miles
+    If the vehicle is not in the database, add the vehicle and set the number of miles to new_miles
+    If the vehicle is None or new_miles is not a positive number, raise MileageError"""
+    vehicle = all_chars_upper_case(vehicle)
 
     if not vehicle:
         raise MileageError('Provide a vehicle name')
-    if not isinstance(new_miles, (int, float))  or new_miles < 0:
+    if not isinstance(new_miles, (int, float)) or new_miles < 0:
         raise MileageError('Provide a positive number for new miles')
 
     conn = sqlite3.connect(db_url)
@@ -32,14 +33,48 @@ def add_miles(vehicle, new_miles):
     conn.close()
 
 
+def all_chars_upper_case(string):
+    # had this in add_miles, but decided to make it more modular and easier to test.
+    return string.upper()
+
+
+def get_mileage(vehicle):
+    # Next 4 lines basically copied from 'add_miles' function
+    conn = sqlite3.connect(db_url)
+    cursor = conn.cursor()
+    result_set = cursor.execute('SELECT total_miles FROM MILES WHERE vehicle = ?', (vehicle,)).fetchall()
+    conn.close()
+    # there should be one or zero results
+    if len(result_set) is 1:
+        # The first item of the only row should be total_miles
+        return str(result_set[0][0])
+    # since there were no results, the car was not found: return 'None'.
+    return None
+
+
 def main():
     while True:
-        vehicle = input('Enter vehicle name or enter to quit:')
+        vehicle = input("To search for an existing vehicle's mileage, enter 'Search'.\n"
+                        "To add mileage to a new or existing vehicle, enter a vehicle name.\n"
+                        "To quit, press 'enter' without a name:\n")
         if not vehicle:
             break
-        miles = float(input('Enter new miles for %s: ' % vehicle)) ## TODO input validation
-
-        add_miles(vehicle, miles)
+        vehicle = all_chars_upper_case(vehicle)
+        # if user takes instructions too literally and puts ''search''
+        # since I did .upper() earlier in the code, this should catch all cases like 'Search' or 'search'
+        if vehicle == "SEARCH" or vehicle == "'SEARCH'":
+            vehicle = all_chars_upper_case(input("Enter name of vehicle to search for it.\n"
+                                                 "Press 'enter' without a name to exit.\n"))
+            if not vehicle:
+                break
+            mileage = get_mileage(vehicle)
+            if not mileage:
+                print('Vehicle was not found.')
+            else:
+                print("Mileage for that vehicle is: \n" + mileage)
+        else:
+            miles = float(input('Enter new miles for %s: ' % vehicle))  # TODO input validation
+            add_miles(vehicle, miles)
 
 
 if __name__ == '__main__':
--- miles_db/test_mileage.py
+++ miles_db/test_mileage.py
@@ -1,19 +1,15 @@
-
 import mileage
 from mileage import MileageError
 import sqlite3
 from unittest import TestCase
 
-class TestMileageDB(TestCase):
 
+class TestMileageDB(TestCase):
     test_db_url = 'test_miles.db'
 
-    """
-    Before running this test, create test_miles.db
+    """Before running this test, create test_miles.db
     Create expected miles table
-    create table miles (vehicle text, total_miles float);
-    """
-
+    create table miles (vehicle text, total_miles float);"""
     # The name of this method is important - the test runner will look for it
     def setUp(self):
         # Overwrite the mileage
@@ -24,31 +20,27 @@ class TestMileageDB(TestCase):
         conn.commit()
         conn.close()
 
-
     def test_add_new_vehicle(self):
         mileage.add_miles('Blue Car', 100)
-        expected = { 'Blue Car': 100 }
+        expected = {'BLUE CAR': 100}
         self.compare_db_to_expected(expected)
 
         mileage.add_miles('Green Car', 50)
-        expected['Green Car'] = 50
+        expected['GREEN CAR'] = 50
         self.compare_db_to_expected(expected)
-
 
     def test_increase_miles_for_vehicle(self):
         mileage.add_miles('Red Car', 100)
-        expected = { 'Red Car': 100 }
+        expected = {'RED CAR': 100}
         self.compare_db_to_expected(expected)
 
         mileage.add_miles('Red Car', 50)
-        expected['Red Car'] = 100 + 50
+        expected['RED CAR'] = 100 + 50
         self.compare_db_to_expected(expected)
-
 
     def test_add_new_vehicle_no_vehicle(self):
         with self.assertRaises(Exception):
             mileage.addMiles(None, 100)
-
 
     def test_add_new_vehicle_invalid_new_miles(self):
         with self.assertRaises(MileageError):
@@ -57,7 +49,6 @@ class TestMileageDB(TestCase):
             mileage.add_miles('Car', 'abc')
         with self.assertRaises(MileageError):
             mileage.add_miles('Car', '12.def')
-
 
     # This is not a test method, instead, it's used by the test methods
     def compare_db_to_expected(self, expected):
@@ -75,3 +66,33 @@ class TestMileageDB(TestCase):
             self.assertEqual(expected[row[0]], row[1])
 
         conn.close()
+
+
+# when I put 'METHOD', it means the method name follows
+class TestMETHODget_mileage(TestCase):
+    # I don't think this is necessary, but it probably makes the code easier to follow.
+    test_db_url = 'test_miles.db'
+
+    # copied next few lines from setUp method at top of file
+    def setUp(self):
+        mileage.db_url = self.test_db_url
+        # drop everything from the DB to always start with an empty database
+        conn = sqlite3.connect(self.test_db_url)
+        conn.execute('DELETE FROM miles')
+        conn.commit()
+        conn.close()
+        # This would be simpler in a list for looping over and
+        # testing each return at end of loop, but instructions
+        # say to keep it here. Probably to practice 'setUp' method.
+        mileage.add_miles('Herbie', 25)
+        mileage.add_miles('Mystery Machine', 27)
+        mileage.add_miles('Bat Mobile', 29)
+
+    # Here, 'METHOD' means I am testing the method in the test name
+    def test_METHOD_search_for_existing_car_check_accurate_return(self):
+        self.assertEqual(mileage.get_mileage('HERBIE'), '25.0')
+        self.assertEqual(mileage.get_mileage('MYSTERY MACHINE'), '27.0')
+        self.assertEqual(mileage.get_mileage('BAT MOBILE'), '29.0')
+
+    def test_GET_MILEAGE_search_for_omitted_car_check_returns_None(self):
+        self.assertEqual(mileage.get_mileage('UFO'), None)
